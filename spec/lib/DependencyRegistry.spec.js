3cc837ce279e0811766298262fffbc45effc7a03
Vitaly Rudenko <maurice.dederick@gmail.com>
2020-09-21 18:19:35 +0300

    Added basic testing config and scripts

---
+++ spec/lib/DependencyRegistry.spec.js
@@ -0,0 +1,26 @@
+const DependencyRegistry = require('../../lib/DependencyRegistry');
+
+describe('DependencyRegistry', () => {
+    /** @type {DependencyRegistry} */
+    let dependencyRegistry;
+
+    beforeEach(() => {
+        dependencyRegistry = new DependencyRegistry();
+    });
+
+    describe('[instances]', () => {
+
+    });
+
+    describe('[factories]', () => {
+
+    });
+
+    describe('[exporting]', () => {
+
+    });
+
+    describe('[importing]', () => {
+
+    });
+});
